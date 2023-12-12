09ae01eecdc29422b6b49d1fecc33dc28a4f4188
Matheus Silva <matheus.r.slv@gmail.com>
2023-12-12 07:22:08 -0300

    Update IoT_Projeto_2.ino

--- Unidade 3/IoT_Projeto_2.ino
+++ Unidade 3/IoT_Projeto_2.ino
@@ -17,11 +17,11 @@
 #define PIN_DOOR 23
 #define LED 2
 
-#define BOTtoken "BOTtoken"
+#define BOTtoken "bottoken"
 // Use @myidbot to find out the chat ID of an individual or a group
 // Also note that you need to click "start" on a bot before it can
 // message you
-#define CHAT_ID "CHAT_ID"
+#define CHAT_ID "chat_id"
 
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
@@ -40,17 +40,19 @@ const char* mqtt_broker = "io.adafruit.com";
 const int mqtt_port = 1883;
 int mqtt_timeout = 10000;
 
-const char* mqtt_usernameAdafruitIO = "username_adafruit";
-const char* mqtt_keyAdafruitIO = "adafruit_key";
+const char* mqtt_usernameAdafruitIO = "usernamee";
+const char* mqtt_keyAdafruitIO = "adafruitkey";
 
 float hum, temp_c, hic = 0;
-int door_state;
-String string_time;
+int doorState;
+int anteriorState = 0;
 char buffer[10];
 unsigned long previousMillis1;  // Variável para armazenar o tempo da última verificação
 unsigned long previousMillis2;  // Variável para armazenar o tempo da última ação (para 10 segundos)
-const long botRequestDelay = 2000;  
+const long botRequestDelay = 1000;  
 const long adafruitDelay = 10000; // Intervalo de 10 segundos
+bool conexionFailed = false;
+String filename = "/dados.txt";
 
 void setup() {
   Serial.begin(115200);
@@ -77,7 +79,7 @@ void loop() {
   hum = dht.readHumidity(); 
   temp_c = dht.readTemperature(); // Read temperature as Celsius (the default) 
 
-  door_state = digitalRead(PIN_DOOR);
+  doorState = digitalRead(PIN_DOOR);
   
   // Check if any reads failed and exit early (to try again). 
   if (isnan(hum) || isnan(temp_c)) {
@@ -85,6 +87,10 @@ void loop() {
     delay(400);
     return; 
   }
+
+  // Verifica se houve mudança de estado
+  doorStateChange();
+  checkTempHum();
 
   // Compute heat index in Celsius (isFahreheit = false) 
   hic = dht.computeHeatIndex(temp_c, hum, false); 
@@ -111,10 +117,21 @@ void loop() {
     connectMQTT();
     if(!mqtt_client.connected()) { // Se MQTT não estiver conectado
       digitalWrite(LED,LOW);
+      bool conexionFailed = true;
+      writeDataFile();
     } else {
       digitalWrite(LED, HIGH); // LED indicando conexão
 
-      Serial.print(F("Humidade: "));
+      if(conexionFailed == true){
+        conexionFailed = false;
+        float mediaTemperatura, mediaUmidade, mediaIndiceCalor;
+        readCalculateFile(&mediaTemperatura, &mediaUmidade, &mediaIndiceCalor);
+        mqtt_client.publish("matheusriv/feeds/projeto2_umidade", String(mediaUmidade).c_str());
+        mqtt_client.publish("matheusriv/feeds/projeto2_temp_c", String(mediaTemperatura).c_str());
+        mqtt_client.publish("matheusriv/feeds/projeto2_hic", String(mediaIndiceCalor).c_str());
+      }
+
+      Serial.print(F("Umidade: "));
       Serial.print(hum);
       Serial.print(F("%  Temperatura: "));
       Serial.print(temp_c);
@@ -122,15 +139,15 @@ void loop() {
       Serial.print(hic);
       Serial.println(F("°C"));
 
-      mqtt_client.publish("username/feeds/projeto2_umidade", String(hum).c_str());
-      mqtt_client.publish("username/feeds/projeto2_temp_c", String(temp_c).c_str());
-      mqtt_client.publish("username/feeds/projeto2_hic", String(hic).c_str());
+      mqtt_client.publish("matheusriv/feeds/projeto2_umidade", String(hum).c_str());
+      mqtt_client.publish("matheusriv/feeds/projeto2_temp_c", String(temp_c).c_str());
+      mqtt_client.publish("matheusriv/feeds/projeto2_hic", String(hic).c_str());
 
-      if(door_state == 1){
-        mqtt_client.publish("username/feeds/projeto2_porta", "1");
+      if(doorState == 1){
+        mqtt_client.publish("matheusriv/feeds/projeto2_porta", "1");
         Serial.println("Porta: fechada");
       } else{
-        mqtt_client.publish("username/feeds/projeto2_porta", "0");
+        mqtt_client.publish("matheusriv/feeds/projeto2_porta", "0");
         Serial.println("Porta: aberta");
       }
 
@@ -142,26 +159,83 @@ void loop() {
   }
 }
 
-void writeFile(String path) {
-  File file = SPIFFS.open(path, "a");
+void checkTempHum(){
+  if(temp_c > 35) {
+    bot.sendMessage(CHAT_ID, "Alerta: temperatura alta " + String(temp_c, 2), "");
+  }
+  if(hum < 40) {
+    bot.sendMessage(CHAT_ID, "Alerta: humidade baixa " + String(hum, 2), "");
+  }
+}
+
+void doorStateChange(){
+  if(doorState != anteriorState) {
+    if(doorState == HIGH) {
+      bot.sendMessage(CHAT_ID, "Alerta: porta fechou", "");
+      Serial.println("Mudança de 0 para 1");
+    } else {
+      bot.sendMessage(CHAT_ID, "Alerta: porta abriu", "");
+      Serial.println("Mudança de 1 para 0");
+    }
+    anteriorState = doorState; // Atualiza o estado anterior com o estado atual
+  }
+}
+
+void writeDataFile() {
+  SPIFFS.remove(filename);
+  File file = SPIFFS.open(filename, "a");
 
   if (!file) {
     Serial.println(F("Falha ao abrir o arquivo no SPIFFS!"));
     return;
   }
 
-  file.print(timeClient.getFormattedTime());
-  file.print(" Porta: ");
-  file.print(door_state);
-  file.print(" Humidade: ");
-  file.print(hum);
-  file.print("%  Temperatura: ");
-  file.print(temp_c);
-  file.print("°C ");
-  file.print(hic);
-  file.println("°C ");
+  // Escreve os dados no arquivo
+  file.println(String(temp_c) + "," + String(hum));
 
   file.close();
+}
+
+void readCalculateFile(float *mediaTemperatura, float *mediaUmidade, float *mediaIndiceCalor){
+  File file = SPIFFS.open(filename, "r");
+
+  if (!file) {
+    Serial.println(F("Falha ao abrir o arquivo no SPIFFS!"));
+    return;
+  }
+
+  float somaTemperatura = 0.0;
+  float somaUmidade = 0.0;
+  float somaIndiceCalor = 0.0;
+  int numDados = 0;
+
+  // Lê os dados do arquivo e calcula a soma para calcular a média posteriormente
+  while (file.available()) {
+    String line = file.readStringUntil('\n');
+    int commaIndex1 = line.indexOf(',');
+    int commaIndex2 = line.indexOf(',', commaIndex1 + 1);
+
+    if (commaIndex1 != -1 && commaIndex2 != -1) {
+      float temp = line.substring(0, commaIndex1).toFloat();
+      float umid = line.substring(commaIndex1 + 1, commaIndex2).toFloat();
+      float heatIndex = line.substring(commaIndex2 + 1).toFloat();
+
+      somaTemperatura += temp;
+      somaUmidade += umid;
+      somaIndiceCalor += heatIndex;
+      numDados++;
+    }
+  }
+
+  file.close();
+
+  // Calcula a média dos valores
+  if (numDados > 0) {
+    *mediaTemperatura = somaTemperatura / numDados;
+    *mediaUmidade = somaUmidade / numDados;
+    *mediaIndiceCalor = somaIndiceCalor / numDados;
+  }
+
 }
 
 void readFile(String path) {
@@ -211,7 +285,6 @@ void handleNewMessages(int numNewMessages) {
       continue;
     }
     
-    // Print the received message
     String text = bot.messages[i].text;
     //Serial.println(text);
 
